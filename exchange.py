2bbc72acf0b0671582d6e93e06e29c8135faf5f8
Diego <didev4207@gmail.com>
2025-04-22 19:08:20 -0400

    Init Commit

---
+++ exchange.py
@@ -0,0 +1,51 @@
+import os, platform
+
+def exchange_money(budget:float, exchange_rate:float) -> float:
+    return round(budget / exchange_rate, 2)
+
+def title() -> None:
+    print('''\033[36;1m
+
+ ▗▄▄▖▗▖ ▗▖▗▄▄▖ ▗▄▄▖ ▗▄▄▄▖▗▖  ▗▖ ▗▄▄▖▗▖  ▗▖    
+▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌   ▐▛▚▖▐▌▐▌    ▝▚▞▘     
+▐▌   ▐▌ ▐▌▐▛▀▚▖▐▛▀▚▖▐▛▀▀▘▐▌ ▝▜▌▐▌     ▐▌      
+▝▚▄▄▖▝▚▄▞▘▐▌ ▐▌▐▌ ▐▌▐▙▄▄▖▐▌  ▐▌▝▚▄▄▖  ▐▌      
+                                              
+                                              
+                                              
+▗▄▄▄▖▗▖  ▗▖ ▗▄▄▖▗▖ ▗▖ ▗▄▖ ▗▖  ▗▖ ▗▄▄▖▗▄▄▄▖    
+▐▌    ▝▚▞▘ ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▛▚▖▐▌▐▌   ▐▌       
+▐▛▀▀▘  ▐▌  ▐▌   ▐▛▀▜▌▐▛▀▜▌▐▌ ▝▜▌▐▌▝▜▌▐▛▀▀▘    
+▐▙▄▄▖▗▞▘▝▚▖▝▚▄▄▖▐▌ ▐▌▐▌ ▐▌▐▌  ▐▌▝▚▄▞▘▐▙▄▄▖    
+                                              
+                                              
+                                              
+
+\033[0m''')
+
+def main() -> None:
+    while True:
+        os.system('cls') if platform.system() == "Windows" else os.system('clear')
+        
+        title()
+
+        try:
+            budget_input = input("\033[34;1mMonto: \033[0m").strip()
+            rate_input = input("\033[34;1mRate: \033[0m").strip()
+            
+            budget = float(budget_input.replace(' ', '').replace(',', ''))
+            rate = float(rate_input.replace(' ', '').replace(',', ''))
+            
+            result: float = exchange_money(budget, rate)
+
+            print(f"{result:,} $")
+            input("\033[33m\nPresiona Enter para Continuar...\n\033[0m")
+        except ValueError as e:
+            print(f"\033[31m\nIngrese valores validos\033[0m")
+            continue
+        except KeyboardInterrupt:
+            print("\033[33m\n\nSaliendo...\033[0m")
+            exit()
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Currency Exchanger
+
+[![](https://img.shields.io/badge/python-3.8%2B-blue)]()
+
+Tool to help Users calculate currency to currency in an efficient way.
+
+## Installation
+
+Just Clone the repository:
+
+```bash
+git clone https://github.com/MyDiDev/Currency-Exchange.git
+```
+
+Move to the new Directory and Execute
+
+```bash
+cd Currency-Exchange
+python3 exchanger.py
+```
+
+## Authors
+
+- [@MyDiDev](https://www.github.com/MyDiDev)
